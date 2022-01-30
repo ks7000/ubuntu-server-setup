648f5fbd44300ed331cbe62383e329a942b45b3e
ks7000.net.ve (#FullSnackDeveloper) <olano@ks7000.net.ve>
2022-01-30 11:53:56 -0400

    Verifica si es usuario raíz.

--- configura-servidor-ubuntu.sh
+++ configura-servidor-ubuntu.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-sudo apt update
-sudo apt install git
 
-cd ~
-git clone https://github.com/ks7000/ubuntu-server-setup.git && cd ~/ubuntu-server-setup && bash ~/ubuntu-server-setup/setup.sh
+# Verifica que sea usuario raíz
+#   https://www.cyberciti.biz/tips/howto-write-shell-script-to-add-user.html
+  if [ $(id -u) -eq 0 ]; then
+    sudo apt update
+    sudo apt install git
+    cd ~
+    git clone https://github.com/ks7000/ubuntu-server-setup.git && cd ~/ubuntu-server-setup && sh ~/ubuntu-server-setup/setup.sh
+else
+	echo "Solo un usuario raíz puede agregar usuarios administradores."
+	exit 2
+fi
