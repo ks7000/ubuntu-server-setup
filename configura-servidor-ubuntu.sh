44a8f3bfd0b5fcbc741b8ba41026c13989cc3007
ks7000.net.ve (#FullSnackDeveloper) <olano@ks7000.net.ve>
2022-01-30 13:34:30 -0400

    Instala Powerline en usuario root.

--- configura-servidor-ubuntu.sh
+++ configura-servidor-ubuntu.sh
@@ -5,6 +5,10 @@
   if [ $(id -u) -eq 0 ]; then
     sudo apt update
     sudo apt install git
+    # Instala Powerline:
+    #	https://colaboratorio.net/jimmy/terminal/2019/como-instalar-powerline-en-ubuntu/
+    sudo apt install fonts-powerline powerline
+    echo 'if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then source /usr/share/powerline/bindings/bash/powerline.sh; fi' | sudo tee -a ~/.bashrc
     cd ~
     rm -R ubuntu-server-setup/
     git clone https://github.com/ks7000/ubuntu-server-setup.git && bash ~/ubuntu-server-setup/setup.sh
